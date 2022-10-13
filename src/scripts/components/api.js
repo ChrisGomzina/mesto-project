6da4ba913162b70df63a2190d7e33ed46b4bf808
Christina Gomzina <chrisgoncharova@yandex.ru>
2022-10-13 18:13:16 +0300

    Refactor Api.js

--- src/scripts/components/api.js
+++ src/scripts/components/api.js
@@ -1,100 +1,111 @@
-export const config = {
-  url: "https://nomoreparties.co/v1/plus-cohort-15",
-  headers: {
-    "content-type": "application/json",
-    "authorization": "9cfce386-84bd-41a8-ae69-e779d7b6a47d"
-  },
-};
-
-//Проверка ответа сервера
-export function checkResponse(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    return Promise.reject(`Ошибка: ${res.status}`);
+export default class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
   }
-};
 
-//Получение информации о пользователе
-export function getUserInfo() {
-  return fetch(`${config.url}/users/me`, {
-    method: 'GET',
-    headers: config.headers
-  })
-  .then(res => checkResponse(res));
-};
+  //Проверка ответа сервера
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  };
 
-//Получение карточек с сервера
-export function getInitialCards() {
-  return fetch(`${config.url}/cards`, {
-     method: 'GET',
-     headers: config.headers
-  })
-  .then(res => checkResponse(res));
-};
-
-//Редактирование профиля
-export function editUserInfo(userName, userJob) {
-  return fetch(`${config.url}/users/me`, {
-    method: 'PATCH',
-    headers: config.headers,
-    body: JSON.stringify({
-      name: userName,
-      about: userJob
+  //Получение информации о пользователе
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'GET',
+      headers: this._headers
     })
-  })
-  .then(res => checkResponse(res));
-};
+    .then(res => _checkResponse(res));
+  };
 
-//Добавление новой карточки
-export function addCard(data) {
-  return fetch(`${config.url}/cards`, {
-     method: 'POST',
-     headers: config.headers,
-     body: JSON.stringify({
+  //Получение карточек с сервера
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'GET',
+      headers: this._headers
+    })
+    .then(res => _checkResponse(res));
+  };
+
+  //Редактирование профиля
+  editUserInfo(userName, userJob) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: userName,
+        about: userJob
+      })
+    })
+    .then(res => _checkResponse(res));
+  };
+
+   //Добавление новой карточки
+  addCard(data) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
         name: data.name,
         link: data.link
       })
-  })
-  .then(res => checkResponse(res));
-};
+    })
+    .then(res => _checkResponse(res));
+  };
 
-//Лайки
-export function likeCard(id) {
-    return fetch(`${config.url}/cards/likes/${id}`, {
+  //Лайки
+  likeCard(id) {
+    return fetch(`${this._baseUrl}/cards/likes/${id}`, {
       method: 'PUT',
-      headers: config.headers
+      headers: this._headers
     })
-    .then(res => checkResponse(res));
+    .then(res => _checkResponse(res));
   };
 
-//Дизлайки
-export function dislikeCard(id) {
-    return fetch(`${config.url}/cards/likes/${id}`, {
+  //Дизлайки
+  dislikeCard(id) {
+    return fetch(`${this._baseUrl}/cards/likes/${id}`, {
       method: 'DELETE',
-      headers: config.headers
+      headers: this._headers
     })
-    .then(res => checkResponse(res));
+    .then(res => _checkResponse(res));
   };
 
-//Удаление карточки
- export function deleteCard(id) {
-    return fetch(`${config.url}/cards/${id}`, {
+  //Удаление карточки
+  deleteCard(id) {
+    return fetch(`${this._baseUrl}/cards/${id}`, {
       method: 'DELETE',
-      headers: config.headers
+      headers: this._headers
     })
-    .then(res => checkResponse(res));
+    .then(res => _checkResponse(res));
   };
 
-//Обновление аватара
-export function editAvatar(avatarURL) {
-  return fetch(`${config.url}/users/me/avatar`, {
+  //Обновление аватара
+  editAvatar(avatarURL) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: 'PATCH',
-      headers: config.headers,
+      headers: this._headers,
       body: JSON.stringify({
         avatar: avatarURL
       })
-  })
-  .then(res => checkResponse(res));
-};
+    })
+    .then(res => _checkResponse(res));
+  };
+
+}
+
+const api = new Api({
+  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-15',
+  headers: {
+    'content-type': 'application/json',
+    'authorization': '9cfce386-84bd-41a8-ae69-e779d7b6a47d'
+  }
+});
+
+
+
 
